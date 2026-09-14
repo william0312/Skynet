721f4eeb569f2f17885c9c1103535b5e92f48442
william0312 <61766321+william0312@users.noreply.github.com>
2026-09-14 13:08:36 -0500

    Update Indicadores_velocidad_git.py

--- Indicadores_velocidad_git.py
+++ Indicadores_velocidad_git.py
@@ -14,8 +14,8 @@ from openpyxl.styles import Font
 
 def ejecutar_cruce_seguro(sitios_activos,velocidad):
 
-    dataframe_1=velocidad.copy()
-    dataframe_2=sitios_activos.copy()
+    dataframe_1 = velocidad.copy()
+    dataframe_2 = sitios_activos.copy()
     matricula1_velocidad_bajada_mostrar = 26.40
     matricula1_velocidad_bajada = 26400
 
