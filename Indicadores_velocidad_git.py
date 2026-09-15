893d0db89f8739040e40c2c5ddc3fcdc603fcd08
william0312 <61766321+william0312@users.noreply.github.com>
2026-09-15 12:24:48 -0500

    Update range filter for dataframe

--- Indicadores_velocidad_git.py
+++ Indicadores_velocidad_git.py
@@ -51,8 +51,8 @@ def ejecutar_cruce_seguro(sitios_activos,velocidad):
     dataframe_1 = dataframe_1[(dataframe_1['Estado'] == 'EN OPERACIÓN')]
     dataframe_1 = dataframe_1[~(dataframe_1['Tipo'] == 'forzada')]
     dataframe_1['Identificador de la prueba'] = dataframe_1['Identificador de la prueba'].astype(str)
-    dataframe_1 = dataframe_1[(dataframe_1['Rango'] >= 6) & (dataframe_1['Rango'] <= 12)]
-    
+    dataframe_1 = dataframe_1[(dataframe_1['Rango'] >= 6) & (dataframe_1['Rango'] <= 20)]
+    print (dataframe_1.shape) 
     '''
     #filtro por matricula
     matricula1 = dataframe_1[dataframe_1['Matricula'].astype(str) == "Matricula <= 50"]
